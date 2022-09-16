10dd29e81eb46df9016e3134d153495d1ce04a36
vitashi <vshisoka@gmail.com>
2022-09-16 11:37:54 +0300

    added a module for utils

---
+++ server/calendar/utils.js
@@ -0,0 +1,16 @@
+const errors = require('./errors');
+
+const validateHostUserID = (userID) => {
+
+    if (!userID) throw new errors.MalformedQueryError('Argument <hostUserId> missing in query!')
+    if (!userIDExists(userID)) throw new errors.HostUserIDNotFoundError(`hostUserId ${userID} not found`)
+
+}
+
+const userIDExists = (userID) => {
+    return true
+}
+
+module.exports = {
+    validateHostUserID
+}

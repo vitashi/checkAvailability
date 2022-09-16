1989f8a711877b9aec558f7b84b87bc41612103d
vitashi <vshisoka@gmail.com>
2022-09-16 22:04:46 +0300

    added return types

--- server/calendar/validator.ts
+++ server/calendar/validator.ts
@@ -3,12 +3,12 @@ import {MalformedQueryError, HostUserIDNotFoundError} from "./errors"
 
 export default class HostUserIDValidator extends BaseValidator{
 
-    override validate(userID?: string){
+    override validate(userID?: string): void{
         if (!userID) throw new MalformedQueryError('Argument <hostUserId> missing in query!')
         if (!this.userIDExists(userID)) throw new HostUserIDNotFoundError(`hostUserId ${userID} not found`)
     }
 
-    userIDExists(userID: string){
+    userIDExists(userID: string): boolean{
         return true
     }
 

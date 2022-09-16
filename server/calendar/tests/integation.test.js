58ec257d9906e1747ab9c941e243953964b5a5d7
vitashi <vshisoka@gmail.com>
2022-09-16 10:52:28 +0300

    added failing test

--- server/calendar/tests/integation.test.js
+++ server/calendar/tests/integation.test.js
@@ -5,7 +5,10 @@
  */
 
 const request = require('supertest');
+const HTTPStatusCodes = require('http-status-codes');
 const app = require('../../app')
+
+const statusCodes = HTTPStatusCodes.StatusCodes
 
 describe('GET /api/calendar', () => {
   it('returns timeslots', async () => {
@@ -18,4 +21,11 @@ describe('GET /api/calendar', () => {
       timeslots: expect.arrayContaining(['2021-11-24T14:00:00.000']),
     });
   });
+  it('returns 404 and json body if the hostUserID is not passed', async () => {
+    const response = await request(app)
+      .get('/api/calendar')
+      .expect('Content-Type', /json/);
+
+    expect(response.statusCode).toEqual(statusCodes.NOT_FOUND);
+  })
 });

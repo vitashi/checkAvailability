5a70e2ccbf069254ab41f8efe1dc764c87261105
vitashi <vshisoka@gmail.com>
2022-09-19 11:56:31 +0300

    added request and response logging using morgan

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import  bodyParser from "body-parser";
 import Users, { User } from "./users/store";
+import morgan from "morgan";
 
 
 const path = require('path');
@@ -16,6 +17,26 @@ const HTML_FILE = path.join(DIST_DIR, 'index.html');
 app.get('/', (req, res) => {
     res.sendFile(HTML_FILE);
   });
+
+// Logs requests
+app.use(morgan(':remote-addr :url :method HTTP/:http-version :user-agent', {
+  // https://github.com/expressjs/morgan#immediate
+  immediate: true,
+  stream: {
+    write: (message) => {
+      console.info(`REQUEST :: ${message.trim()}`);
+    }
+  }
+}));
+
+// Logs responses
+app.use(morgan(':remote-addr :url :method :status :res[content-length] :response-time ms', {
+  stream: {
+    write: (message) => {
+      console.info(`RESPONSE :: ${message.trim()}`);
+    }
+  }
+}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
